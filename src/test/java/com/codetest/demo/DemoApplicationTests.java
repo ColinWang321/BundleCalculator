61e36ba48a67e674fca56a2b1a6df5930e2bb87b
Colin Wang <colinwang321@gmail.com>
2022-01-08 04:21:21 +0800

    Deleted old test code.

--- src/test/java/com/codetest/demo/DemoApplicationTests.java
+++ src/test/java/com/codetest/demo/DemoApplicationTests.java
@@ -14,25 +14,8 @@ class DemoApplicationTests {
 
 	@Test
 	void testCorrectOutput() {
-		String str = bundle.bestBundle(14, "IMG");
-		logger.info("Test Correct Output result: " + str);
+
 	}
 
-	@Test
-	void testWrongNumber() {
-		String str = bundle.bestBundle(-1, "IMG");
-		logger.info("Test Wrong Number result: " + str);
-	}
 
-	@Test
-	void testWrongType() {
-		String str = bundle.bestBundle(23, "IMGG");
-		logger.info("Test Wrong Type result: " + str);
-	}
-
-	@Test
-	void testNullType() {
-		String str = bundle.bestBundle(23, "");
-		logger.info("Test Null Type result: " + str);
-	}
 }
