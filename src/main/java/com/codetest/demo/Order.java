a44999acce78549bf23de8cd80823a806eb83f1e
Colin Wang <colinwang321@gmail.com>
2022-01-08 23:34:00 +0800

    Change ability name from "formats" to "types".

--- src/main/java/com/codetest/demo/Order.java
+++ src/main/java/com/codetest/demo/Order.java
@@ -6,7 +6,7 @@ import lombok.Getter;
 @Getter
 public class Order {
 
-    String[] format = {"IMG", "FLAC", "VID"};
+    String[] types = {"IMG", "FLAC", "VID"};
     int[][] bundles = {{10, 5}, {9, 6, 3}, {9, 5, 3}};
     double[][] prices = {{800, 450}, {1147.50, 810, 427.50}, {1530, 900, 570}};
 
