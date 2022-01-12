25ac3a6b82f48680cd02a49adcb951eb245e9de9
Colin Wang <colinwang321@gmail.com>
2022-01-13 00:13:55 +0800

    Code in main function changed a little.

--- src/main/java/com/codetest/demo/DemoApplication.java
+++ src/main/java/com/codetest/demo/DemoApplication.java
@@ -1,20 +1,15 @@
 package com.codetest.demo;
 
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-
-
 public class DemoApplication {
-
-    static Logger logger = LoggerFactory.getLogger(DemoApplication.class);
 
     public static void main(String[] args) {
 
+        Calculator calculator = new Calculator();
+        Order order = calculator.getInput("bundle.txt");
         Bundle bundle = new Bundle();
 
-        String str = bundle.calBestBundlesFromTxtFile("bundle.txt");
-        logger.info(str);
+        bundle.writeStringToTxt(order, calculator);
 
 
     }
