280d1e79c6299fe5a35afc435c69b683ebbbbc64
Colin Wang <colinwang321@gmail.com>
2022-01-13 00:11:29 +0800

    Change name to BundleInfo.java.

---
+++ src/main/java/com/codetest/demo/BundleInfo.java
@@ -0,0 +1,10 @@
+package com.codetest.demo;
+
+import lombok.Getter;
+
+@Getter
+public class BundleInfo {
+    String[] types = {"IMG", "FLAC", "VID"};
+    int[][] bundles = {{10, 5}, {9, 6, 3}, {9, 5, 3}};
+    double[][] prices = {{800, 450}, {1147.50, 810, 427.50}, {1530, 900, 570}};
+}
